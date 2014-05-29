6dd9c3924822a86510fe3807e7a1fd425486af81
Tom Dror <tdror53@gmail.com>
2014-05-28 20:44:16 -0400

    moved things to packages, working on controller

---
+++ src/dominantspecies/model/Tile.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominantspecies.model;
+
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+
+/**
+ *
+ * @author DJ 4 Fathers, Toms Over Baghdad
+ */
+public class Tile extends Polygon {
+    
+    public static enum TerrainType {
+
+        Tundra,
+        Forest,
+        Jungle,
+        Mountain,
+        Desert,
+        Sea,
+        Wetlands,
+        Savannah,
+        Invalid
+    }
+    
+    private TerrainType terrain;
+    
+    public Tile(TerrainType terrain, int[] cx, int[] cy) {
+        super(cx,cy,6);
+        this.terrain = terrain; 
+    }
+    public void drawTile(Graphics2D g2){
+        //TODO have tile draw itself
+    }
+    public int[] Species = new int[6];
+ 
+    
+    public TerrainType getTerrain(){
+        return this.terrain;
+    }
+    
+    public void setTerrain(TerrainType terrain){
+        this.terrain=terrain;
+    }
+    public int[] ScoreValues(){
+        switch(this.terrain) {
+            case Sea:
+              return new int[] { 9, 5, 3, 2 };
+            case Wetlands:
+              return new int[] { 8, 4, 2, 1 };
+            case Savannah:
+              return new int[] { 7, 4, 2 };
+            case Jungle:
+              return new int[] { 6, 3, 2 };
+            case Forest:
+              return new int[] { 5, 3, 2 };
+            case Desert:
+              return new int[] { 4, 2 };
+            case Mountain:
+              return new int[] { 3, 2 };
+            case Tundra:
+              return new int[] { 1 };
+          }
+          return new int[] {};
+    }
+    
+    public int getSpeciateCount(){
+        switch(this.terrain) {
+            case Sea:
+            case Wetlands:
+              return 4;
+            case Savannah:
+            case Jungle:
+            case Forest:
+              return 3;
+            case Desert:
+            case Mountain:
+              return 2;
+            case Tundra:
+              return 1;
+          }
+          return 0;
+    }
+    
+    public String getImageName(){    
+        //highley reccomend moving this to the view classes
+        return "images/"+this.getTerrain()+".jpeg";
+    }
+         
+
+}
