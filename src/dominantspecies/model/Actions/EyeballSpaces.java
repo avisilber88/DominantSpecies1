68b44b13e2c359d02935ef3e47a5bfbd5ce4b9d6
Tom Dror <tdror53@gmail.com>
2014-06-10 08:53:24 -0400

    changed to an array avi i may need to talk to you about this

--- src/dominantspecies/model/Actions/EyeballSpaces.java
+++ src/dominantspecies/model/Actions/EyeballSpaces.java
@@ -13,45 +13,60 @@ import java.util.ArrayList;
 /**
  *
  * @author Tom
+ * 
+ * talk to me about this class as it may need to change depending on how you 
+ * use it.
  */
 public class EyeballSpaces {
-    private final ArrayList<EyeballSpace> queue;//represents the player in the queue
+    private final EyeballSpace[] queue;//represents the player in the queue
     private final int sizeLimit;
-    private int last = 0;
+    private int first,last;//keep track of positions in the queue
     
     public EyeballSpaces(int sizeLimit){
         this.sizeLimit = sizeLimit;
-        this.queue = new ArrayList(sizeLimit);
+        this.queue = new EyeballSpace[sizeLimit];
         for(int i = 0; i < sizeLimit; i++)//initialize to all null
-            queue.add(i, null);
+            queue[i] =  null;
+        first = 0;
+        last = 0;
+    }
+    /////////use this when needed to be treated like a queue///////////
+    public EyeballSpace removeNext(){
+        EyeballSpace returnTo = queue[first];
+        queue[first] = null;
+        first++;
+        return returnTo;
     }
     
-    public EyeballSpace getNext(){
-        if(!queue.isEmpty())
-            return queue.remove(0);
-        last--;
-        return null;
+     public EyeballSpace getNext(){
+        return queue[first];
     }
-    
-    public EyeballSpace get(int i){
-        return queue.get(i);
-    }
-    
-    public boolean add(EyeballSpace player){
-        if(last < sizeLimit){
-            queue.add(last, player);
+     
+     public boolean add(EyeballSpace player){
+        if(last < sizeLimit && queue[last] == null){
+            queue[last] = player;
             last++;
             return true;
         }
         return false;        
     }
+     /////////////////////
+     
+    public EyeballSpace remove(int i){
+        EyeballSpace returnTo = queue[i];
+        queue[i] = null;
+        return returnTo;
+    }
+   
+    
+    public EyeballSpace get(int i){
+        return queue[i];
+    }    
     
     public boolean add(EyeballSpace player, int i){
-        if(i >=0 && i < sizeLimit){
-            if(queue.get(i) == null){
-                queue.add(i, player);
-                return true;
-            }
+        if(i >=0 && i < sizeLimit && queue[i] == null){
+            queue[i] = player;
+            return true;
         }
         return false;
     }
