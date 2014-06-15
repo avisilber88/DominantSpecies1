ff84fe8289683d699c1744f5d4b4ced5d42b4ffa
Tom Dror <tdror53@gmail.com>
2014-06-15 02:31:26 -0400

    almost fixed the set up, then going to get the controller then you can FINALLY get the rest working

--- src/dominantspecies/model/Board.java
+++ src/dominantspecies/model/Board.java
@@ -6,13 +6,12 @@
 
 package dominantspecies.model;
 
-import javax.swing.JPanel;
 
 /**
  *
  * @author DJ 4 Fathers
  */
-public class Board extends JPanel {
+public class Board {
 
      // public static boolean XYVertex=false;
     //  final static Tile EMPTY = new Tile(Tile.TerrainType.Invalid);
@@ -20,8 +19,8 @@ public class Board extends JPanel {
    // final static int HEXSIZE = 60;	//hex size in pixels
     //final static int BORDERS = 15;
     //final static int SCRSIZE = 525;//HEXSIZE * (BSIZE + 1) + BORDERS * 3; //screen size (vertical dimension).
-    Tile[][] board;
-    ElementTile[][] boardElements;   
+    final Tile[][] board;
+    final ElementTile[][] boardElements;   
 
     public Board(boolean defaultSetup) {
         board = new Tile[BSIZE][BSIZE - 1];
@@ -34,54 +33,55 @@ public class Board extends JPanel {
     //sets up the grids and sets the tiles to invalid
     //setupBoard() will set the initial game tiles and element things
     public final void initBoard() {
-        for (int i = 0; i < BSIZE; i++) {
-            for (int j = 0; j < (BSIZE - 1); j++) {         
-                board[i][j] = new Tile(Tile.TerrainType.Invalid);
-
-                if (boardElements[2 * i + 1][j] == null) {
-                    boardElements[2 * i + 1][j] = new ElementTile(ElementType.None);
-                } else {
-                    System.out.println("see there was a copy");
-                }
-                if (boardElements[2 * i + 2][j] == null) {
-                    boardElements[2 * i + 2][j] = new ElementTile(ElementType.None);
-                } else {
-                    System.out.println("see there was a copy");
-                }
-                if (boardElements[2 * i + 3][j + i % 2] == null) {
-                    boardElements[2 * i + 3][j + i % 2] = new ElementTile(ElementType.None);
-                } else {
-                    System.out.println("see there was a copy");
-                }
-                if (boardElements[2 * i + 2][j + 1] == null) {
-                    boardElements[2 * i + 2][j + 1] = new ElementTile(ElementType.None);
-                } else {
-                    System.out.println("see there was a copy");
-                }
-                if (boardElements[2 * i + 1][j + 1] == null) {
-                    boardElements[2 * i + 1][j + 1] = new ElementTile(ElementType.None);
-                } else {
-                    System.out.println("see there was a copy");
-                }
-                if (boardElements[2 * i][j + i % 2] == null) {
-                    boardElements[2 * i][j + i % 2] = new ElementTile(ElementType.None);
-                } else {
-                    System.out.println("see there was a copy");
-                }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {        
+                board[i][j] = new Tile(TerrainType.None);
+                if(!hexExist(i, j))
+                    board[i][j].setTerrain(TerrainType.Invalid);               
+               /*
+                else{                    
+                    if (boardElements[2 * i + 1][j] == null) {
+                        boardElements[2 * i + 1][j] = new ElementTile(ElementType.None);
+                    }
+                    if (boardElements[2 * i + 2][j] == null) {
+                        boardElements[2 * i + 2][j] = new ElementTile(ElementType.None);
+                    }
+                    if (boardElements[2 * i + 3][j + i % 2] == null) {
+                        boardElements[2 * i + 3][j + i % 2] = new ElementTile(ElementType.None);
+                    }
+                    if (boardElements[2 * i + 2][j + 1] == null) {
+                        boardElements[2 * i + 2][j + 1] = new ElementTile(ElementType.None);
+                    }
+                    if (boardElements[2 * i + 1][j + 1] == null) {
+                        boardElements[2 * i + 1][j + 1] = new ElementTile(ElementType.None);
+                    }
+                    if (boardElements[2 * i][j + i % 2] == null) {
+                        boardElements[2 * i][j + i % 2] = new ElementTile(ElementType.None);
+                    }                   
+                } 
+                */
             }
-        }        
+        } 
+        for (int i = 0; i < boardElements.length; i++) {
+            for (int j = 0; j < boardElements[i].length; j++) { 
+                boardElements[i][j] = new ElementTile(ElementType.None);
+               // if(!hexExist(i/2, j/3))
+               //     boardElements[i][j].setElementType(ElementType.Invalid);
+            }
+        }
+        
     }
     
-    public final void defaultSettings(){
-        board[3][1].setTerrain(Tile.TerrainType.Wetlands);
-        board[4][2].setTerrain(Tile.TerrainType.Savannah);
-        board[4][3].setTerrain(Tile.TerrainType.Desert);
-        board[3][3].setTerrain(Tile.TerrainType.Mountain);
-        board[2][3].setTerrain(Tile.TerrainType.Forest);
-        board[2][2].setTerrain(Tile.TerrainType.Jungle);
+    private void defaultSettings(){
+        board[3][1].setTerrain(TerrainType.Wetlands);
+        board[4][2].setTerrain(TerrainType.Savannah);
+        board[4][3].setTerrain(TerrainType.Desert);
+        board[3][3].setTerrain(TerrainType.Mountain);
+        board[2][3].setTerrain(TerrainType.Forest);
+        board[2][2].setTerrain(TerrainType.Jungle);
 
-        board[3][2].setTerrain(Tile.TerrainType.Tundra);
-
+        board[3][2].setTerrain(TerrainType.Tundra);
+/*
         boardElements[7][3].setElementType(ElementType.Meat);
         boardElements[8][3].setElementType(ElementType.Sun);
         boardElements[9][4].setElementType(ElementType.Meat);
@@ -96,10 +96,10 @@ public class Board extends JPanel {
 
         boardElements[8][2].setElementType(ElementType.Water);
         boardElements[9][2].setElementType(ElementType.Grass);
+    */   
     }
     
-    //dont think we need this method here?
-    public boolean checkHexExist(int x, int y) {
+    public boolean hexExist(int x, int y) {
         if ((x == 0) && (y == 0)) {
             return false;
         } else if ((x == 6) && (y == 0)) {
@@ -118,4 +118,15 @@ public class Board extends JPanel {
             return true;
         }
     }
+
+    public Tile[][] getBoard() {
+        return board;
+    }
+
+
+    public ElementTile[][] getBoardElements() {
+        return boardElements;
+    }
+
+    
 }
